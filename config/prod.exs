f5a65ba3ac5496463a856dbbdc6e160e5ee2ca21
Gary Haran <gary.haran@gmail.com>
2017-01-14 14:43:49 -0500

    fixing screwups

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :faq, Faq.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "polycurious.herokuapp.com", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :faq, Faq.Repo,
