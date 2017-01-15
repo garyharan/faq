25c48d457f838bf842176f906ab4ecaac40a8e37
Gary Haran <gary.haran@gmail.com>
2017-01-15 09:20:44 -0500

    fixing url redirect

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :faq, Faq.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "polycurious.herokuapp.com", port: 443],
+  url: [scheme: "http", host: "polycurio.us", port: 80],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
